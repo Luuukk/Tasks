2442d93f4156335c7ce1c27d4538fd6c38ab5be2
Luca Fernandes <lukavbf3rnandes@hotmail.com>
2024-06-12 19:39:09 -0300

    Tasksv4

--- app/src/main/java/com/devmasterteam/tasks/service/repository/BaseRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/BaseRepository.kt
@@ -1,6 +1,10 @@
 package com.devmasterteam.tasks.service.repository
 
 import android.content.Context
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import com.devmasterteam.tasks.R
 import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.listener.APIListener
@@ -29,6 +33,32 @@ open class BaseRepository(val context: Context) {
             }
 
         })
+    }
+
+    fun isConnectionAvailable(): Boolean {
+        var result = false
+
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val activeNet = cm.activeNetwork ?: return false
+            val netWorkCapabilities = cm.getNetworkCapabilities(activeNet) ?: return false
+            result = when {
+                netWorkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                netWorkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else {
+            if (cm.activeNetworkInfo != null) {
+                result = when (cm.activeNetworkInfo!!.type) {
+                    ConnectivityManager.TYPE_WIFI -> true
+                    ConnectivityManager.TYPE_MOBILE -> true
+                    ConnectivityManager.TYPE_ETHERNET -> true
+                    else -> false
+                }
+            }
+        }
+
+        return result
     }
 
 }
